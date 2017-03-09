9edbe8ba9de3369fb4096a2eeaf02a5cc56de5d5
NewMC00 <david.w.mueller@ndus.edu>
2017-03-09 15:13:51 -0600

    New project "WhereIsWaldo?"

---
+++ WhereIsWaldo/WhereIsWaldo.cpp
@@ -0,0 +1,15 @@
+// WhereIsWaldo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Where's Waldo?\n\n";
+	cout << "Where in the world is Waldo?";
+
+    return 0;
+}
